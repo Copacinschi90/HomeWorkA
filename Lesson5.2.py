eb3843b8bc2d59b8697e734208f0817a5bc563c2
copacinschi90 <>
2022-05-20 13:15:25 -0400

    Please review.Thank you

---
+++ Lesson5.2.py
@@ -0,0 +1,7 @@
+def show_employee(employee_name, salary=50000):
+    print(employee_name, salary)
+
+    show_employee(employee_name="Maria", Salary=80000)
+
+    show_employee("Vadim", 70000)
+    show_employee("Marin")
