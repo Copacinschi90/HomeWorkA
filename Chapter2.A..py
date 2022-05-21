c5391288378a7498825c10696f8ba59b319cf068
copacinschi90 <>
2022-05-21 15:03:10 -0400

    Chapter 2

---
+++ Chapter2.A..py
@@ -0,0 +1,14 @@
+
+word_test = "Test Automation"
+
+print(word_test[:4])
+
+
+second_word = "Test Automation"
+
+print(second_word[-10:])
+
+
+sliced_list = [10,50,20,25,30]
+
+print(sliced_list [1:3])
